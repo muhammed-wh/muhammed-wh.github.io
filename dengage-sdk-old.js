61b3b5fc85037eead750578c550b49b8378a8eb5
Muhammed Emin Aydın <muhammed@whitehorse.technology>
2019-10-23 11:31:02 +0300

    test

--- dengage-sdk-old.js
+++ dengage-sdk-old.js
@@ -61,9 +61,7 @@
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'omit', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'text/plain'
-      },
+      headers: { 'Content-Type': 'application/json' },
       redirect: 'follow', // manual, *follow, error
       referrer: 'no-referrer', // no-referrer, *client
       body: JSON.stringify(data) // body data type must match "Content-Type" header
