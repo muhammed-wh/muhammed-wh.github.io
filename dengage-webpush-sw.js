279e8f39ce97ef1f063c52b1f41f9b77d97dac6c
Muhammed Emin Aydın <muhammed@whitehorse.technology>
2019-12-03 14:44:05 +0300

    test

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -1,1 +1,1 @@
-importScripts("https://appdev.dengage.com/push/3/7916b426-b69e-abe0-eacd-ba768086a9a7/dengage_sw.js");
+importScripts("https://pdev.dengage.com/p/push/3/7916b426-b69e-abe0-eacd-ba768086a9a7/dengage_sw.js");
