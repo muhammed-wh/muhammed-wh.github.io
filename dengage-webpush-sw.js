65323313ff58c7fb1f82e714c6d94a3249eb8c2e
muhammed-wh <56356454+muhammed-wh@users.noreply.github.com>
2021-10-05 11:48:12 +0300

    Update dengage-webpush-sw.js

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -2,7 +2,7 @@
 var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
+var domain = swUrl.searchParams.get('domain') || '';
 if (accountId && appGuid) {
-    importScripts("https://pdev.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+    importScripts("https://" + domain + "/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
-        
